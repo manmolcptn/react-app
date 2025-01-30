41a11e59c050ddc19ac86dac3f7864ff0ad57541
manmolcptn <molinacapitanmiguelangel@gmail.com>
2025-01-30 14:49:59 +0100

    react

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import {useRef, useState} from "react";
 import Iterador from "./components/iterador/iterador.js";
 import Selector from "./components/selector/selector.js";
 import Sumar from "./components/sumar/sumar.js";
+import img from "./img/imagen1.jpg";
 
 function App() {
   
-
   return (
     <> 
-      <Iterador/>
+      <img src={img}/>
     </>
   );
 }
