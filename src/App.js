684ce7a23d2b5928b21fdf19d0ebcd8d2d0e3e1c
Miguel Ángel Molina <168438229+manmolcptn@users.noreply.github.com>
2025-01-30 20:59:15 +0100

    repaso

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import {useRef} from "react";
 import Iterador from "./components/iterador/iterador.js";
 import Selector from "./components/selector/selector.js";
 import Sumar from "./components/sumar/sumar.js";
-import imagen1 from "./img/imagen1.jpg";
+import Imagen from './components/imagen/imagen.js';
+import Imagenes from './components/imagenes/imagenes.js';
+
 
 function App() {
-  //Para acceder al DOM usamos el hook useRef
-  const foto = useRef();
-
-  const cambiarFoto = () => {
-    let src = foto.current.src
-    //No confundir include, con includes.
-    //El primero es para arrays, el segundo para strings.
-    if(src.includes("imagen1")){
-      foto.current.src
-    }
-
-  }
+  
 
   return (
     <> 
-      <img src={imagen1} onClick={cambiarFoto} ref={foto}/>
+      <Imagenes/>
+      <Imagenes/>
     </>
   );
 }
