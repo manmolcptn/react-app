0ecb29a25b10f3bbcaca4753a92daacd12faee44
manmolcptn <molinacapitanmiguelangel@gmail.com>
2025-01-30 14:40:45 +0100

    principios de react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useRef, useState} from "react";
+import Iterador from "./components/iterador/iterador.js";
+import Selector from "./components/selector/selector.js";
+import Sumar from "./components/sumar/sumar.js";
+
+function App() {
+  
+
+  return (
+    <> 
+      <Iterador/>
+    </>
+  );
+}
+
+export default App;
